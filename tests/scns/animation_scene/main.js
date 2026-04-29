ed2c71dbc59f65bf7f29cc1f527753ae8b750486
supchyan <mikhailsupov@yandex.ru>
2026-04-29 12:17:22 +0300

    better tests section

--- tests/scns/animation_scene/main.js
+++ tests/scns/animation_scene/main.js
@@ -8,5 +8,16 @@ AnimationManager.load(element, {
     timingFunction: AnimationManager.TimingFunction.cubicBezier(0, -3, 1, 4),
     direction:      AnimationManager.Direction.Alternate
 });
+
+setTimeout(() => { // replace animation after some time
+    AnimationManager.load(element, { 
+        name:           "testAnimation2", 
+        duration:       500,
+        iterationCount: AnimationManager.IterationCount.Infinite,
+        timingFunction: AnimationManager.TimingFunction.cubicBezier(.25, 0, .25, 1),
+        direction:      AnimationManager.Direction.Alternate
+    });
+}, 3000);
+
 // just testing the hex color parsing here
 console.info(Color.fromHEX("#f10f"));
